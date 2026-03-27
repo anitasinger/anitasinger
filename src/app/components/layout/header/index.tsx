ff2b7d9fe525132c9126f2e9dde8cefe6f0e398e
iamsanjayjoshi <80983225+iamsanjayjoshi@users.noreply.github.com>
2026-03-28 02:30:43 +0530

    just updated more data

--- src/app/components/layout/header/index.tsx
+++ src/app/components/layout/header/index.tsx
@@ -22,7 +22,7 @@ const Header = () => {
               className="relative overflow-hidden cursor-pointer w-fit h-full py-2 sm:py-3 md:py-5 px-4 sm:px-5 md:px-7 border border-primary rounded-full group"
             >
               <span className="relative z-10 text-xl font-medium text-black group-hover:text-white transition-colors duration-300">
-                Download PDF Resume
+                Book Now
               </span>
             </Button>
           </div>
